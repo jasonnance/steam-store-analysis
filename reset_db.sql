f5e667e1a46025ff32547b21c95d523f888cc399
Jason Nance <acro6spino9@yahoo.com>
2017-04-13 22:56:19 -0400

    Add more DB tables for game fields, some scraping code
    Still missing a few parts we want to pull off the page.

--- reset_db.sql
+++ reset_db.sql
@@ -1,8 +1,83 @@
 DROP TABLE IF EXISTS game;
 
 CREATE TABLE game (
-    steam_app_id int NOT NULL,
+    steam_app_id int,
     game_name text,
 
     CONSTRAINT game_pk PRIMARY KEY (steam_app_id)
 );
+
+DROP TABLE IF EXISTS game_crawl;
+
+CREATE TABLE game_crawl (
+    steam_app_id int,
+    crawl_time timestamp with time zone DEFAULT (now() at time zone 'utc'),
+    game_name text,
+    short_description text,
+    reviews_last_30_days int,
+    pct_positive_reviews_last_30_days real,
+    reviews_all_time int,
+    pct_positive_reviews_all_time real,
+    release_date date,
+    title text,
+    genre text,
+    developer text,
+    publisher text,
+    num_achievements int,
+    full_price real,
+    long_description text,
+
+    CONSTRAINT game_crawl_pk PRIMARY KEY (steam_app_id, crawl_time),
+    CONSTRAINT game_game_crawl_fk FOREIGN KEY (steam_app_id)
+        REFERENCES game (steam_app_id)
+);
+
+DROP TABLE IF EXISTS steam_tag;
+
+CREATE TABLE steam_tag (
+    tag_id serial,
+    descr text,
+
+    CONSTRAINT steam_tag_pk PRIMARY KEY (tag_id),
+    CONSTRAINT steam_tag_descr_uniq UNIQUE (descr)
+);
+
+DROP TABLE IF EXISTS game_crawl_tag;
+
+CREATE TABLE game_crawl_tag (
+    steam_app_id int,
+    crawl_time timestamp with time zone,
+    tag_id int,
+
+    CONSTRAINT game_crawl_tag_pk PRIMARY KEY (steam_app_id, crawl_time, tag_id),
+    CONSTRAINT game_crawl_game_crawl_tag_fk FOREIGN KEY (steam_app_id, crawl_time)
+        REFERENCES game_crawl (steam_app_id, crawl_time),
+    CONSTRAINT steam_tag_game_crawl_tag_fk FOREIGN KEY (tag_id)
+        REFERENCES steam_tag (tag_id)
+)
+
+DROP TABLE IF EXISTS steam_game_detail;
+
+CREATE TABLE steam_game_detail (
+     detail_id serial,
+     descr text,
+
+    CONSTRAINT steam_game_detail_pk PRIMARY KEY (detail_id),
+    CONSTRAINT steam_game_detail_descr_uniq UNIQUE (descr)
+);
+
+DROP TABLE IF EXISTS game_crawl_detail;
+
+CREATE TABLE game_crawl_detail (
+    steam_app_id int,
+    crawl_time timestamp with time zone,
+    detail_id int,
+
+    CONSTRAINT game_crawl_detail_pk PRIMARY KEY (steam_app_id, crawl_time, detail_id),
+    CONSTRAINT game_crawl_game_crawl_detail_fk FOREIGN KEY (steam_app_id, crawl_time)
+        REFERENCES game_crawl (steam_app_id, crawl_time),
+    CONSTRAINT steam_detail_game_crawl_detail_fk FOREIGN KEY (detail_id)
+        REFERENCES steam_detail (detail_id)
+);
+
+
--- scrape.py
+++ scrape.py
@@ -2,10 +2,21 @@ from selenium import webdriver
 import requests
 import dataset
 import os
+import re
 
-db = dataset.connect(os.environ['POSTGRES_URI'])
+# Number of seconds to sleep between crawls
+CRAWL_TIMEOUT = 10
+
+THIRTY_DAY_REVIEW_REGEX = re.compile(r'^([0-9]+)% of the ([,0-9]+) user reviews in the last 30 days')
+ALL_TIME_REVIEW_REGEX = re.compile(r'^([0-9]+)% of the ([,0-9]+) user reviews for this game')
+DETAILS_BOX_REGEX = re.compile(r'^Title: ([^\n]+)\nGenre: ([^\n]+)\nDeveloper: ([^\n]+)\nPublisher: ([^\n]+)')
+NUM_ACHIEVEMENTS_REGEX = re.compile(r'Includes ([,[0-9]+) Steam Achievements')
 
 def upsert_all_apps():
+    '''
+    Get the full list of steam apps and upsert them in our database
+    on the basis of steam's app ID.
+    '''
     json = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v0001/').json()
 
     apps = json['applist']['apps']['app']
@@ -16,10 +27,106 @@ def upsert_all_apps():
             'game_name': app['name'],
         }, keys=['app_id'])
 
-# Since we're not super worried about having an up-to-date list of apps,
-# run this only if the table is empty
-if db['game'].count() == 0:
-    upsert_all_apps()
+def scrape_store_page(app_id):
+    '''
+    Extract all the information we can from the store page for a given app ID.
+    '''
+    results = {}
+    driver = webdriver.Chrome()
+    driver.get("http://store.steampowered.com/app/{}".format(app_id))
 
-# driver = webdriver.Chrome()
-# driver.get("http://store.steampowered.com")
+    results['game_name'] = (driver
+                            .find_element_by_class_name('apphub_AppName')
+                            .text)
+
+    results['short_description'] = (driver
+                                    .find_element_by_class_name('game_description_snippet')
+                                    .text)
+
+    reviews_texts = [element.get_attribute('data-store-tooltip')
+                     for element in (driver
+                                     .find_elements_by_class_name('user_reviews_summary_row')
+                                     )]
+
+    for text in reviews_texts:
+        thirty_day_match = THIRTY_DAY_REVIEW_REGEX.match(text)
+
+        if thirty_day_match:
+            results['pct_positive_reviews_last_30_days'] = int(thirty_day_match.group(1))
+            results['reviews_last_30_days'] = int(thirty_day_match.group(2).replace(',', ''))
+        else:
+            all_time_match = ALL_TIME_REVIEW_REGEX.match(text)
+
+            if all_time_match:
+                results['pct_positive_reviews_all_time'] = int(all_time_match.group(1))
+                results['reviews_all_time'] = int(all_time_match.group(2).replace(',', ''))
+
+    results['release_date'] = driver.find_element_by_css_selector('.release_date .date').text
+
+    details_text = driver.find_elements_by_class_name('details_block')[0].text
+    details_match = DETAILS_BOX_REGEX.match(details_text)
+    results['title'] = details_match.group(1)
+    results['genre'] = details_match.group(2)
+    results['developer'] = details_match.group(3)
+    results['publisher'] = details_match.group(4)
+
+    block_titles_texts = [element.text
+                          for element in (driver
+                                          .find_elements_by_class_name('block_title')
+                                          )]
+
+    for text in block_titles_texts:
+        num_achievements_match = NUM_ACHIEVEMENTS_REGEX.match(text)
+
+        if num_achievements_match:
+            results['num_achievements'] = int(num_achievements_match.group(1))
+
+    # TODO
+    # - full_price
+    # - long_description
+    # - game_details (ex. "Single-player", "Multi-player", etc)
+    # - tags (ex. "Strategy", "4X", "Space", etc)
+
+    return results
+
+def do_crawl(app_ids, db):
+    '''
+    Given a list of steam app IDs and a db connection, do a crawl for the app IDs
+    and append the results to our list of crawls in the database.
+    '''
+    for app_id in app_ids:
+        results = scrape_store_page(app_id)
+
+        db['game_crawl'].insert(results)
+
+        time.sleep(CRAWL_TIMEOUT)
+
+
+def run():
+    db = dataset.connect(os.environ['POSTGRES_URI'], ensure_schema=False)
+
+    # Since we're not super worried about having an up-to-date list of apps,
+    # run this only if the table is empty
+    if db['game'].count() == 0:
+        upsert_all_apps()
+
+    # For now, just crawl the apps we don't already have
+    missing_crawl_query = '''
+    SELECT steam_app_id
+    FROM game g
+      LEFT JOIN game_crawl gc
+        USING (steam_app_id)
+    WHERE gc.steam_app_id IS NULL;
+    '''
+
+    missing_app_ids = [r['steam_app_id'] for r in db.query(missing_crawl_query)]
+
+    # TODO remove after we're done testing the crawl function
+    missing_app_ids = missing_app_ids[:1]
+
+    do_crawl(missing_app_ids)
+
+
+if __name__ == '__main__':
+    run()
+
