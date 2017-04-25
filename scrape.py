7228289ee7af21c9279106547819c3165ad64492
jason.m.nance <jason.m.nance@gmail.com>
2017-04-25 17:27:25 -0400

    Account for missing app_tag element
    Usually it's just invisible if there aren't a full list of tags,
    but looks like sometimes it's not in the DOM at all.

--- scrape.py
+++ scrape.py
@@ -213,7 +213,7 @@ def scrape_store_page(app_id):
             # Try to get the big list of tags if it's there
             driver.find_element_by_css_selector('.app_tag.add_button').click()
             tag_elements = driver.find_elements_by_css_selector('#app_tagging_modal a.app_tag')
-        except ElementNotVisibleException:
+        except (NoSuchElementException, ElementNotVisibleException):
             # Settle for the short list if not
             tag_elements = driver.find_elements_by_css_selector('a.app_tag')
         for element in tag_elements:
